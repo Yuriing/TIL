e85f2d18dbef5fa9536e77e960f74867252439bb
Yuriing <b98w020@gmail.com>
2025-03-12 23:12:31 +0900

    250312_2

--- 250303/1211_ladder2.py
+++ 250303/1211_ladder2.py
@@ -1,44 +1,47 @@
 import sys
 sys.stdin = open('input1211.txt')
 
-# for _ in range(1, 11):
-#     T = int(input())
+for _ in range(1, 11):
+    T = int(input())
 
-#     matrix = []
-#     for _ in range(100):
-#         matrix.append(list(map(int, input().split())))
+    matrix = []
+    for _ in range(100):
+        matrix.append(list(map(int, input().split())))
 
-#     # 좌, 우, 하
-#     dr = [0, 0, 1]
-#     dc = [-1, 1, 0]
+    # 좌, 우, 하
+    dr = [0, 0, 1]
+    dc = [-1, 1, 0]
 
-#     count = 0
-#     max_v = 0
-#     for j in range(100):
-#         if matrix[0][j] == 1:
-#             r, c = 0, j
-#             matrix[0][j] = 3
+    count = 0
+    max_v = 300
+    answer_j = 0
+    for j in range(100):
+        if matrix[0][j] == 1:
+            r, c = 0, j
+            visited = [row[:] for row in matrix]
+            visited[0][j] = 3
 
-#             while r < 99:
-#                 for i in range(3):
-#                     if 0 <= r+dr[i] < 100 and 0 <= c+dc[i] < 100:
-#                         nr = r + dr[i]
-#                         nc = c + dc[i]
+            while r < 99:
+                for i in range(3):
+                    if 0 <= r+dr[i] < 100 and 0 <= c+dc[i] < 100:
+                        nr = r + dr[i]
+                        nc = c + dc[i]
 
-#                         if matrix[nr][nc] == 1:
-#                             matrix[nr][nc] = 3
-#                             count += 1
-#                             r = nr
-#                             c = nc
-#                             break
-#                         else:
-#                             continue
+                        if matrix[nr][nc] == 1:
+                            visited[nr][nc] = 3
+                            count += 1
+                            r = nr
+                            c = nc
+                            break
+                        else:
+                            continue
             
-#             if max_v < count:
-#                 max_v = count
+            if max_v <= count:
+                max_v = count
+                answer_j = j
 
 
-#     print(f'{T} {max_v}')
+    print(f'#{T} {answer_j}')
 
 # for _ in range(1, 11):
 #     T = int(input())
@@ -84,46 +87,46 @@ sys.stdin = open('input1211.txt')
 
 #     print(f'#{T} {answer_j}')
 
-for _ in range(1, 11):
-    T = int(input())
+# for _ in range(1, 11):
+#     T = int(input())
 
-    matrix = []
-    for _ in range(100):
-        matrix.append(list(map(int, input().split())))
+#     matrix = []
+#     for _ in range(100):
+#         matrix.append(list(map(int, input().split())))
 
-    min_distance = float('inf')
-    answer_j = 0
+#     min_distance = float('inf')
+#     answer_j = 0
 
-    for j in range(100):
-        if matrix[0][j] == 1:
-            r, c = 0, j
-            visited = [row[:] for row in matrix]  # matrix 원본 유지용 복사본
-            visited[r][c] = 2  # 방문 처리
-            distance = 1
+#     for j in range(100):
+#         if matrix[0][j] == 1:
+#             r, c = 0, j
+#             visited = [row[:] for row in matrix]  # matrix 원본 유지용 복사본
+#             visited[r][c] = 2  # 방문 처리
+#             distance = 1
 
-            while r < 99:
-                # 왼쪽으로 이동할 수 있으면 계속 이동
-                if c > 0 and visited[r][c - 1] == 1:
-                    while c > 0 and visited[r][c - 1] == 1:
-                        c -= 1
-                        distance += 1
-                        visited[r][c] = 2
+#             while r < 99:
+#                 # 왼쪽으로 이동할 수 있으면 계속 이동
+#                 if c > 0 and visited[r][c - 1] == 1:
+#                     while c > 0 and visited[r][c - 1] == 1:
+#                         c -= 1
+#                         distance += 1
+#                         visited[r][c] = 2
 
-                # 오른쪽으로 이동할 수 있으면 계속 이동
-                elif c < 99 and visited[r][c + 1] == 1:
-                    while c < 99 and visited[r][c + 1] == 1:
-                        c += 1
-                        distance += 1
-                        visited[r][c] = 2
+#                 # 오른쪽으로 이동할 수 있으면 계속 이동
+#                 elif c < 99 and visited[r][c + 1] == 1:
+#                     while c < 99 and visited[r][c + 1] == 1:
+#                         c += 1
+#                         distance += 1
+#                         visited[r][c] = 2
 
-                # 좌우가 막히면 아래로 이동
-                r += 1
-                distance += 1
-                visited[r][c] = 2
+#                 # 좌우가 막히면 아래로 이동
+#                 r += 1
+#                 distance += 1
+#                 visited[r][c] = 2
 
-            # 최단 거리 갱신
-            if distance < min_distance:
-                min_distance = distance
-                answer_j = j
+#             # 최단 거리 갱신
+#             if distance < min_distance:
+#                 min_distance = distance
+#                 answer_j = j
 
-    print(f'#{T} {answer_j}')
+#     print(f'#{T} {answer_j}')
--- 250312/5189_전자카트.py
+++ 250312/5189_전자카트.py
@@ -1,2 +1,36 @@
-lst = [i for i in range(2, 6)]
-print(lst)
+import sys
+sys.stdin = open('input5189.txt')
+
+# 들를 관리구역 번호와 그에 따른 배터리 소모량의 최소값을 구하는 함수
+def perm(selected, remain, current):
+    global min_battery
+    # 만일 현재 배터리 소모량이 이미 현재의 최소 배터리 소모량을 넘어섰을 경우 현재 경로 탐색을 종료한다
+    if current > min_battery:
+        return
+
+    if not remain:  # 관리구역을 다 돌았을 때 사무실로 돌아오게 한다
+        current += matrix[selected[-1]-1][0]
+        min_battery = min(current, min_battery)
+        return
+
+    for i in range(len(remain)):  # 들르게 되는 관리구역에 따라 배터리 소모량을 측정한다
+        pick = remain[i]
+        new_remain = remain[:i] + remain[i+1:]
+        perm(selected+[pick], new_remain, current+matrix[selected[-1]-1][pick-1])
+
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+
+    matrix = []
+    for _ in range(N):
+        matrix.append(list(map(int, input().split())))
+
+    # 2부터 N까지의 자연수를 모두 뽑아 num 리스트 생성
+    num = [i for i in range(2, N+1)]
+    min_battery = float('inf')
+
+    perm([1], num, 0)
+
+    print(f'#{tc} {min_battery}')
